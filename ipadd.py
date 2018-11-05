5cd77ff15063268c276c39996c48a14fe549773d
sairam8882 <43983254+sairam8882@users.noreply.github.com>
2018-11-05 14:49:35 +0530

    Update ipadd.py

--- ipadd.py
+++ ipadd.py
@@ -3,3 +3,5 @@ import socket
 def respond_to_sample(rammbock, msg, client):
     hostname = socket.gethostname()
     IPAddr = socket.gethostbyname(hostname)
+    print("Your Computer Name is:" + hostname)    
+    print("Your Computer IP Address is:" + IPAddr)
