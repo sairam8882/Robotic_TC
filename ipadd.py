3e0d4ad40c9e91f5809aa1f7bd395e149fd8d474
sairam8882 <43983254+sairam8882@users.noreply.github.com>
2018-11-05 17:07:19 +0530

    Update ipadd.py

--- ipadd.py
+++ ipadd.py
@@ -1,8 +1,8 @@
 import socket 
 def getipaddress():
     print("Your Computer IP Address is:")
-    hostname = socket.gethostname(google.com)
-    IPAddr = socket.gethostbyname(hostname)
+    #hostname = socket.gethostname(google.com)
+    IPAddr = socket.gethostbyname(google.com)
     print("Your Computer Name is:" + hostname)    
     print("Your Computer IP Address is:" + IPAddr)
     return IPAddr
